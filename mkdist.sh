7620d48771324211b3e347b7812e6c2b1563ee70
Gertjan Halkes <hg@ghalkes.nl>
2011-10-28 16:09:34 +0200

    Add DESTDIR support and LIBVERSION note in Makefile.in

--- mkdist.sh
+++ mkdist.sh
@@ -28,6 +28,9 @@ sed -i "/#define T3_CONFIG_VERSION/c #define T3_CONFIG_VERSION ${VERSION_BIN}" $
 OBJECTS="`echo \"${SOURCES} ${GENSOURCES} ${AUXSOURCES}\" | tr ' ' '\n' | sed -r 's%\.objects/%%' | egrep '^src/.*\.c$' | sed -r 's/\.c\>/.lo/g' | tr '\n' ' '`"
 
 #FIXME: somehow verify binary compatibility, and print an error if not compatible
+LIBVERSION="${VERSIONINFO%%:*}"
+
+sed -r -i "s%<LIBVERSION>%${LIBVERSION}%g" ${TOPDIR}/Makefile.in
 
 sed -r -i "s%<OBJECTS>%${OBJECTS}%g;\
 s%<VERSIONINFO>%${VERSIONINFO}%g" ${TOPDIR}/Makefile.in
